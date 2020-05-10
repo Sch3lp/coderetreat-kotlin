feba026e0449588864423553dbe151a0ca3a0876
sch3lp <sch3lp@gmail.com>
2020-05-10 17:53:39 +0200

    Extracted checksum validation into its own fun

--- src/main/kotlin/be/swsb/coderetreat/VinValidator.kt
+++ src/main/kotlin/be/swsb/coderetreat/VinValidator.kt
@@ -5,29 +5,32 @@ import java.util.*
 
 object VinValidator {
     private val actualMap = listOf('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
-                       .zip(listOf( 1,   2,   3,   4,   5,   6,   7,   8,   0,   1,   2,   3,   4,   5,   0,   7,   0,   9,   2,   3,   4,   5,   6,   7,   8,   9,   0,   1,   2,   3,   4,   5,   6,   7,   8,   9))
-                       .toMap()
+            .zip(listOf(1, 2, 3, 4, 5, 6, 7, 8, 0, 1, 2, 3, 4, 5, 0, 7, 0, 9, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9))
+            .toMap()
     private val weights = intArrayOf(8, 7, 6, 5, 4, 3, 2, 10, 0, 9, 8, 7, 6, 5, 4, 3, 2)
 
     fun validate(vinToValidate: String?): Optional<ValidationError> {
-        val cleanedUpVin = vinToValidate.strippedOfDashesBlanksAndUppercased() ?: return Optional.of(ValidationError.from("VIN_MANDATORY"))
+        val cleanedUpVin = vinToValidate.strippedOfDashesBlanksAndUppercased()
+                ?: return Optional.of(ValidationError.from("VIN_MANDATORY"))
         if (cleanedUpVin.length != 17) return Optional.of(ValidationError.from("VIN_MAX_LENGTH"))
         if (cleanedUpVin.containsIllegalCharacters()) return Optional.of(ValidationError.from("VIN_ILLEGAL_CHARACTER"))
+        if (hasInvalidChecksum(cleanedUpVin)) return Optional.of(ValidationError.from("VIN_ILLEGAL"))
+        return Optional.empty<ValidationError>()
+    }
 
-        var checkSum = cleanedUpVin.foldIndexed(0) { i, acc, c ->
+    private fun hasInvalidChecksum(cleanedUpVin: String): Boolean {
+        val checkSum = cleanedUpVin.foldIndexed(0) { i, acc, c ->
             val value = actualMap[c] ?: 0
             acc + weights[i] * value
         }
-        // check digit
-        checkSum = checkSum % 11
-        val checkCharacter = cleanedUpVin[8]
-        if (checkSum == 10 && checkCharacter == 'X') {
-            return Optional.empty<ValidationError>()
-        }
-        return if (checkSum == transliterate(checkCharacter)) {
-            Optional.empty<ValidationError>()
-        } else Optional.of(ValidationError.from("VIN_ILLEGAL"))
+        val mod = checkSum % 11
+        return !isValidChecksum(mod, cleanedUpVin.getCheckCharacter())
     }
+
+    private fun isValidChecksum(mod: Int, checkCharacter: Char) =
+            isValidSpecialCase(mod, checkCharacter) || isValidTransliteration(mod, checkCharacter)
+    private fun isValidSpecialCase(mod: Int, checkCharacter: Char) = mod == 10 && checkCharacter == 'X'
+    private fun isValidTransliteration(mod: Int, checkCharacter: Char) = mod == transliterate(checkCharacter)
 
     private fun transliterate(check: Char): Int {
         if (check == 'A' || check == 'J') {
@@ -55,12 +58,14 @@ object VinValidator {
     }
 }
 
+private fun String.getCheckCharacter() = this[8]
 private fun String?.strippedOfDashesBlanksAndUppercased() = this
         ?.replace("-".toRegex(), "")
         ?.replace(" ".toRegex(), "")
         ?.toUpperCase()
         ?.ifBlank { null }
+
 private fun String.containsIllegalCharacters() = this.any { it.isIllegalCharacter() }
 private fun Char.isAlphaNumerical() = "[A-Z0-9]*".toRegex().matches("$this")
 private fun Char.isNotAlphaNumerical() = !this.isAlphaNumerical()
-private fun Char.isIllegalCharacter() = this.isNotAlphaNumerical() || this in listOf('I','O','Q')
+private fun Char.isIllegalCharacter() = this.isNotAlphaNumerical() || this in listOf('I', 'O', 'Q')
