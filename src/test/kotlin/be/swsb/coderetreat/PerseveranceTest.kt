f0e50788a86395aa2bcd21e47202a6008bbdccce
Tim Schraepen <sch3lp@gmail.com>
2021-02-18 21:28:22 +0100

    🎉 Perseverance can move forwards, up the Y axis

--- src/test/kotlin/be/swsb/coderetreat/PerseveranceTest.kt
+++ src/test/kotlin/be/swsb/coderetreat/PerseveranceTest.kt
@@ -1,5 +1,6 @@
 package be.swsb.coderetreat
 
+import be.swsb.coderetreat.MarsRoverCommand.Forwards
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -11,17 +12,54 @@ class PerseveranceTest {
         val landedPerseverance = land(Position(0, 0), "North")
 
         assertThat(landedPerseverance)
-            .isEqualTo(MarsRover(name = "Perseverance", pos = Position(0,0), facing = "North"))
+            .isEqualTo(Perseverance(pos = Position(0, 0), facing = "North"))
+    }
+
+    @Test
+    fun `Perseverance can receive a forwards command and move towards a new position`() {
+        val landedPerseverance = land(Position(0, 0), "North")
+
+        val updatedRover = landedPerseverance.receive(Forwards)
+
+        assertThat(updatedRover)
+            .isEqualTo(Perseverance(pos = Position(x = 0, y = 1), facing = "North"))
+    }
+
+    @Test
+    fun `Perseverance can receive a forwards command twice and move towards a new position`() {
+        val landedPerseverance = land(Position(0, 0), "North")
+
+        val updatedRover: Perseverance = landedPerseverance
+            .receive(Forwards)
+            .receive(Forwards)
+
+        assertThat(updatedRover)
+            .isEqualTo(Perseverance(pos = Position(x = 0, y = 2), facing = "North"))
+    }
+
+}
+
+data class Perseverance(
+    val pos: Position,
+    val facing: String,
+) {
+    fun receive(command: MarsRoverCommand): Perseverance {
+        return copy(pos = moveUpTheYAxis())
+    }
+
+    private fun moveUpTheYAxis() = this.pos.moveUpTheYAxis(1)
+}
+
+data class Position(val x: Int, val y: Int) {
+    fun moveUpTheYAxis(step: Int): Position {
+        return this.copy(y = this.y + step)
     }
 }
 
-fun land(landingPosition: Position, facingDirection: String): MarsRover {
-    return MarsRover("Perseverance", pos = landingPosition, facing = facingDirection)
+enum class MarsRoverCommand {
+    Forwards
 }
 
-data class Position(val x: Int, val y: Int)
-data class MarsRover(
-    val name: String = "Perseverance",
-    val pos: Position,
-    val facing: String,
-)
+fun land(landingPosition: Position, facingDirection: String): Perseverance {
+    return Perseverance(pos = landingPosition, facing = facingDirection)
+}
