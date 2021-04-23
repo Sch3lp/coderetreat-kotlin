204bf06221729e9c374b9f5390b32cae5c432e1b
Sofie <sofie.muys@external.toyota-europe.com>
2021-04-23 15:38:59 +0200

    Implemented moving south

--- src/test/kotlin/be/swsb/coderetreat/RoverTest.kt
+++ src/test/kotlin/be/swsb/coderetreat/RoverTest.kt
@@ -9,7 +9,7 @@ class RoverTest {
 
     @Test
     fun `A Rover can be initiated with a starting position and a starting facing direction`() {
-        val initiatedRover = defaultRover()
+        val initiatedRover = Rover()
         assertThat(initiatedRover).isEqualTo(Rover(at = Position(0, 0), facing = Direction.North))
     }
 
@@ -17,26 +17,28 @@ class RoverTest {
     inner class ReceivingForwards {
         @Test
         fun `A Rover facing North, moves up the Y axis`() {
-            val initiatedRover = defaultRover().receive(Command.Forwards)
-            assertThat(initiatedRover).isEqualTo(Rover(at = Position(0, 1), facing = Direction.North))
+            val initiatedRover = Rover().receive(Command.Forwards).receive(Command.Forwards)
+            assertThat(initiatedRover).isEqualTo(Rover(at = Position(0, 2), facing = Direction.North))
         }
 
         @Test
-        fun `A Rover facing North, receiving forward twice, moves up the Y axis twice`() {
-            val initiatedRover = defaultRover().receive(Command.Forwards).receive(Command.Forwards)
-            assertThat(initiatedRover).isEqualTo(Rover(at = Position(0, 2), facing = Direction.North))
+        fun `A Rover facing South, moves down the Y axis`() {
+            val initiatedRover = Rover(facing = Direction.South).receive(Command.Forwards).receive(Command.Forwards)
+            assertThat(initiatedRover).isEqualTo(Rover(at = Position(0, -2), facing = Direction.South))
         }
     }
 }
 
 data class Rover(private val at: Position = Position(0, 0), private val facing: Direction = Direction.North) {
     fun receive(command: Command): Rover {
-        return this.copy(at = at.moveUp())
+        val moveDirection = facing
+        return this.copy(at = this.at.move(moveDirection))
     }
 }
 
 enum class Direction {
-    North
+    North,
+    South
 }
 
 enum class Command {
@@ -44,9 +46,19 @@ enum class Command {
 }
 
 data class Position(private val x: Int, private val y: Int) {
-    fun moveUp(): Position {
+    fun move(moveDirection: Direction): Position {
+        return if (moveDirection == Direction.North) {
+            moveUp()
+        } else {
+            moveDown()
+        }
+    }
+
+    private fun moveUp(): Position {
         return this.copy(y = this.y + 1)
     }
-}
 
-fun defaultRover() = Rover()
+    private fun moveDown(): Position {
+        return this.copy(y = this.y - 1)
+    }
+}
