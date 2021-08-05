276046a62973ff781b51a169e37e8bd8397069f4
Tim Schraepen <sch3lp@gmail.com>
2021-08-05 22:37:15 +0200

    initial test

---
+++ src/test/kotlin/be/swsb/coderetreat/AbsenceCalculationEngineTest.kt
@@ -0,0 +1,38 @@
+package be.swsb.coderetreat
+
+import be.swsb.coderetreat.WorkType.Work
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+
+
+private val July16 = LocalDate.of(2018, 7, 16)
+
+class AbsenceCalculationEngineTest {
+
+    @Test
+    fun `Given employee is expected to work 8 hours on July 16th, then sees 8 hours of work on July 16th`() {
+        val report = calculate(July16)
+
+        assertThat(report.on(July16))
+            .isEqualTo(8 hoursOf Work)
+    }
+
+}
+
+fun calculate(date: LocalDate): Report {
+    return mapOf()
+}
+
+// report
+typealias Report = Map<LocalDate,Pair<Int, WorkType>>
+
+fun Report.on(date: LocalDate) : Pair<Int, WorkType>? = this[date]
+
+enum class WorkType {
+    Work
+}
+
+// helper
+infix fun Int.hoursOf(worktype: WorkType) = this to worktype
+
