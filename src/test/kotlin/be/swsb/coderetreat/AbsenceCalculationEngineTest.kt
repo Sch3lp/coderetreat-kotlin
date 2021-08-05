e9d960f1df9b7ba84c74a9cb47ea5e88dd5c19ee
Tim Schraepen <sch3lp@gmail.com>
2021-08-05 23:08:48 +0200

    changed `calculate` to `addAbsencesOn`, because that's how the assignment actually works

--- src/test/kotlin/be/swsb/coderetreat/AbsenceCalculationEngineTest.kt
+++ src/test/kotlin/be/swsb/coderetreat/AbsenceCalculationEngineTest.kt
@@ -2,26 +2,37 @@ package be.swsb.coderetreat
 
 import be.swsb.coderetreat.WorkType.Work
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
-import kotlin.jvm.JvmInline
 
 
 private val July16 = LocalDate.of(2018, 7, 16)
 
 class AbsenceCalculationEngineTest {
 
-    @Test
-    fun `Given employee is expected to work 8 hours on July 16th, then sees 8 hours of work on July 16th`() {
-        val report = calculate(July16)
+    @Nested
+    inner class `Given employee is expected to work 8 hours on July 16th` {
+        @Test
+        fun `when employee didn't add absences, then sees 8 hours of work on July 16th`() {
+            val report = addAbsencesOn(July16)
 
-        assertThat(report.on(July16))
-            .isEqualTo(8 hoursOf Work)
+            assertThat(report.on(July16))
+                .isEqualTo(8 hoursOf Work)
+        }
+        @Test
+        fun `when employee adds absences, then sees 8 hours of work on July 16th`() {
+            val report = addAbsencesOn(July16)
+
+            assertThat(report.on(July16))
+                .isEqualTo(8 hoursOf Work)
+        }
     }
+
 
 }
 
-fun calculate(date: LocalDate): Report {
+fun addAbsencesOn(date: LocalDate, vararg hours: Hours): Report {
     return mapOf()
 }
 
@@ -35,15 +46,9 @@ enum class WorkType {
 }
 
 // helper
-infix fun Int.hoursOf(worktype: WorkType) = Hours(this to worktype)
+infix fun Int.hoursOf(worktype: WorkType) = Hours(this, worktype)
 
-@JvmInline
-value class Hours(private val value: Pair<Int, WorkType>) {
-    val hours: Int
-        get() = value.first
-    val type: WorkType
-        get() = value.second
-
+data class Hours(private val hours: Int, private val type: WorkType) {
     override fun toString() = "$hours hours of $type"
 }
 
