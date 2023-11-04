2d4f4b06744eadb9204d1905bdb170e874be7b01
Sch3lp <sch3lp@gmail.com>
2023-11-04 13:22:53 +0100

    Succeeding test for Point.rangeTo on y axis; exposed bug in render

--- src/test/kotlin/be/swsb/coderetreat/BattleshipTest.kt
+++ src/test/kotlin/be/swsb/coderetreat/BattleshipTest.kt
@@ -175,8 +175,17 @@ fun renderPoint(renderPoint: Point, carrierAt: Point?): String =
 
 data class Point(val x: Int, val y: Int) {
     operator fun rangeTo(other: Point) : List<Point> {
-        val pointsBetween = if (this.x > other.x) this.x - other.x else other.x - this.x
-        return (this.x.. this.x + pointsBetween).map { this.copy(x = it) }
+        return when {
+            this.y == other.y -> {
+                val pointsBetween = if (this.x > other.x) this.x - other.x else other.x - this.x
+                (this.x..this.x + pointsBetween).map { this.copy(x = it) }
+            }
+            this.x == other.x -> {
+                val pointsBetween = if (this.y > other.y) this.y - other.y else other.y - this.y
+                (this.y..this.y + pointsBetween).map { this.copy(y = it) }
+            }
+            else -> emptyList()
+        }
     }
 
     operator fun plus(other: Point) =
