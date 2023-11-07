575335ad0a33ce3e86c51971125786cbc3cffdc0
Sch3lp <sch3lp@gmail.com>
2023-11-07 20:00:07 +0100

    Now by making impossible states impossible by using the type system <3

--- src/test/kotlin/be/swsb/coderetreat/GameTest.kt
+++ src/test/kotlin/be/swsb/coderetreat/GameTest.kt
@@ -1,40 +1,21 @@
 package be.swsb.coderetreat
 
-import org.assertj.core.api.Assertions.assertThatExceptionOfType
-import org.assertj.core.api.Assertions.assertThatNoException
 import org.junit.jupiter.api.Test
 
 class GameTest {
 
     @Test
     fun `A Game of Battleship can only be started when there are 2 players`() {
-        val game = Game()
-
-        assertThatExceptionOfType(IllegalStateException::class.java)
-            .isThrownBy { game.start() }
-            .withMessage("You require 2 players to be able to play a game of Battleship")
-
-        game.ready(playerOne = "Bruce", playerTwo = "Selina")
-
-        assertThatNoException()
-            .isThrownBy { game.start() }
+        Game.start(playerOne = "Bruce", playerTwo = "Selina")
     }
 }
 
-class Game {
-    var playerOne: String? = null
-        private set
-    var playerTwo: String? = null
-        private set
+class Game private constructor(
+    private val playerOne: String,
+    private val playerTwo: String,
+) {
 
-    fun start() {
-        check(playerOne != null && playerTwo != null) {
-            "You require 2 players to be able to play a game of Battleship"
-        }
-    }
-
-    fun ready(playerOne: String, playerTwo: String) {
-        this.playerOne = playerOne
-        this.playerTwo = playerTwo
+    companion object {
+        fun start(playerOne: String, playerTwo: String) = Game(playerOne, playerTwo)
     }
 }
