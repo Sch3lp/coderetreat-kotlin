fd45e7c408b61c819051d41b42e348c1438c5eef
Sch3lp <sch3lp@gmail.com>
2023-11-07 20:44:39 +0100

    Cleaned up test setup a bit

--- src/test/kotlin/be/swsb/coderetreat/GameTest.kt
+++ src/test/kotlin/be/swsb/coderetreat/GameTest.kt
@@ -1,8 +1,8 @@
 package be.swsb.coderetreat
 
+import be.swsb.coderetreat.Direction.*
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.Test
-import kotlin.reflect.KClass
 
 class GameTest {
 
@@ -16,46 +16,51 @@ class GameTest {
     fun `A Game of Battleship requires both players to have placed all their ships before they can start firing at each other`() {
         val game = Game.start("Bruce", "Selina")
         assertThatExceptionOfType(IllegalStateException::class.java)
-            .isThrownBy { game.fire(target = game.playerTwo, point = Point(1,1)) }
+            .isThrownBy { game.fire(target = game.playerTwo, point = Point(1, 1)) }
 
-        val player1PlacedShips = game
-            .place(game.playerOne, PatrolBoat, Point(1,1), Direction.Horizontally)
-            .place(game.playerOne, Submarine, Point(1,2), Direction.Horizontally)
-            .place(game.playerOne, Destroyer, Point(1,3), Direction.Horizontally)
-            .place(game.playerOne, Battleship, Point(1,4), Direction.Horizontally)
-            .place(game.playerOne, Carrier, Point(1,5), Direction.Horizontally)
-
-        val bothPlayersPlacedShips = player1PlacedShips
-            .place(game.playerTwo, PatrolBoat, Point(1,1), Direction.Vertically)
-            .place(game.playerTwo, Submarine, Point(2,1), Direction.Vertically)
-            .place(game.playerTwo, Destroyer, Point(3,1), Direction.Vertically)
-            .place(game.playerTwo, Battleship, Point(4,1), Direction.Vertically)
-            .place(game.playerTwo, Carrier, Point(5,1), Direction.Vertically)
+        val bothPlayersPlacedShips = game
+            .withAllShipsPlacedInTopLeftCorner(game.playerOne, Horizontally)
+            .withAllShipsPlacedInTopLeftCorner(game.playerTwo, Vertically)
 
         assertThatNoException()
             .isThrownBy {
-                bothPlayersPlacedShips.fire(bothPlayersPlacedShips.playerTwo, Point(1,1))
+                bothPlayersPlacedShips.fire(bothPlayersPlacedShips.playerTwo, Point(1, 1))
             }
     }
 
 }
 
+private fun Game.withAllShipsPlacedInTopLeftCorner(player: Player, direction: Direction): Game =
+    Ship::class.sealedSubclasses
+        .mapNotNull { it.objectInstance }
+        .foldIndexed(this) { index, game, ship ->
+            game.place(
+                player = player,
+                ship = ship,
+                startingPoint = when (direction) {
+                    Horizontally -> Point(1, index + 1)
+                    Vertically -> Point(index + 1, 1)
+                },
+                direction = direction
+            )
+        }
+
 data class Game private constructor(
     val playerOne: Player,
     val playerTwo: Player,
-    private val playerOneField: PlayerField = PlayerField(),
-    private val playerTwoField: PlayerField = PlayerField(),
+    val playerOneField: PlayerField = PlayerField(),
+    val playerTwoField: PlayerField = PlayerField(),
 ) {
 
     fun fire(target: Player, point: Point): Game {
-        check (playerOneField.isComplete() && playerTwoField.isComplete())
+        check(playerOneField.isComplete() && playerTwoField.isComplete())
         return this
     }
 
-    fun place(player: Player, ship: Ship, startingPoint: Point, direction: Direction) : Game {
-        return when(player) {
-            is Player1 -> copy(playerOneField = playerOneField.place(ship,startingPoint, direction))
-            is Player2 -> copy(playerTwoField = playerTwoField.place(ship,startingPoint, direction))
+    fun place(player: Player, ship: Ship, startingPoint: Point, direction: Direction): Game {
+        return when (player) {
+            is Player1 -> copy(playerOneField = playerOneField.place(ship, startingPoint, direction))
+            is Player2 -> copy(playerTwoField = playerTwoField.place(ship, startingPoint, direction))
         }
     }
 
@@ -76,6 +81,7 @@ sealed class Player(name: String, default: String) {
 
         return name == other.name
     }
+
     override fun hashCode(): Int {
         return name.hashCode()
     }
