1832dd9db94bb4bf9515f31a99f0878bfa2bea0f
Sch3lp <sch3lp@gmail.com>
2023-11-07 20:04:02 +0100

    Test that verifies a game cannot be played with an unnamed player

--- src/test/kotlin/be/swsb/coderetreat/GameTest.kt
+++ src/test/kotlin/be/swsb/coderetreat/GameTest.kt
@@ -1,21 +1,30 @@
 package be.swsb.coderetreat
 
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import java.lang.IllegalArgumentException
 
 class GameTest {
 
     @Test
-    fun `A Game of Battleship can only be started when there are 2 players`() {
-        Game.start(playerOne = "Bruce", playerTwo = "Selina")
+    fun `A Game of Battleship cannot be started without player names`() {
+        Assertions.assertThatExceptionOfType(IllegalArgumentException::class.java)
+            .isThrownBy { Game.start(playerOne = "Bruce", playerTwo = "") }
     }
 }
 
 class Game private constructor(
-    private val playerOne: String,
-    private val playerTwo: String,
+    private val playerOne: Player,
+    private val playerTwo: Player,
 ) {
 
     companion object {
-        fun start(playerOne: String, playerTwo: String) = Game(playerOne, playerTwo)
+        fun start(playerOne: String, playerTwo: String) = Game(Player(playerOne), Player(playerTwo))
+    }
+}
+
+data class Player(val name: String) {
+    init {
+        require(name.isNotBlank()) { "Cannot have blank as a player name" }
     }
 }
